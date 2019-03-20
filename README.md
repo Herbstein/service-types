b0f8001cb0b4e6b3fe2c8403cdbee61a603402df
Rasmus Larsen <doctorherbstein@gmail.com>
2019-03-21 00:03:36 +0100

    Working multi-file multi-url definitions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # service-types
+
+The program loads the file `test.json` from the working directory and parses it.
+
--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,56 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
 
 module Main where
 
 import           Control.Applicative
 import           Data.Aeson
 import qualified Data.ByteString.Lazy as B
+import           Data.Functor
+import           Data.List
 import           Data.Maybe
 import           Data.Text (Text)
+import           Debug.Trace
 import           GHC.Generics
 import           Lib
 import           Prelude
+import           System.FilePath
 
-data TypeFile = TypeFile { name :: Text, urls :: [Text] } deriving (Show, Generic)
+data TypeFile = TypeFile { name :: String, urls :: [String] }
+  deriving (Show, Generic, ToJSON, FromJSON)
 
-instance FromJSON TypeFile
-instance ToJSON TypeFile
+typeFile :: FilePath
+typeFile = "test.json"
 
+getJSON :: IO B.ByteString
+getJSON = B.readFile typeFile
 
+data TypeClasses = TypeClasses { classesName :: String, classes :: [Class] }
+  deriving (Show)
+
+parseTypeFile :: TypeFile -> IO TypeClasses
+parseTypeFile file = TypeClasses (name file) . catMaybes
+  <$> mapM parseServiceNowAPIDocumentation (urls file)
+
+data TypeDefinitionFile =
+  TypeDefinitionFile { fileName :: String, contents :: String }
+
+typeClassesToFile :: TypeClasses -> TypeDefinitionFile
+typeClassesToFile xs = TypeDefinitionFile (classesName xs)
+  $ intercalate "\n\n" (map prettyPrint $ classes xs)
+
+writeDefinitionFile :: FilePath -> TypeDefinitionFile -> IO ()
+writeDefinitionFile basePath file = writeFile (basePath </> fileName file <.> "d.ts") (contents file)
 
 main :: IO ()
 main = do
-  cl <- parseServiceNowAPIDocumentation
-    "https://docs.servicenow.com/bundle/jakarta-application-development/page/app-store/dev_portal/API_reference/glideRecordScoped/concept/c_GlideRecordScopedAPI.html#c_GlideRecordScopedAPI"
-  putStrLn $ maybe "" prettyPrint cl
+  fileDecode <- eitherDecode <$> getJSON
+  files <- case fileDecode of
+    Left err    -> do
+      putStrLn err
+      return []
+    Right files -> return files
+  fileClasses <- mapM parseTypeFile files
+  let definitionFiles = map typeClassesToFile fileClasses
+  mapM_ (writeDefinitionFile "examples") definitionFiles
